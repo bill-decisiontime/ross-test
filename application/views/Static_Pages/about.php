5510cfcd0bf3d210c0a924ba0ff92dc8bac0741c
ross-teamsolutionz <ross.walker@teamsolutionz.com>
2017-02-06 12:07:16 +0000

    Documented Files
    Added comments to files to know what they do.

--- application/views/Static_Pages/about.php
+++ application/views/Static_Pages/about.php
@@ -1,2 +1,5 @@
+//This is the View for testing static pages.
+//It was written using a CodeIgnitor tutorial found here:- https://codeigniter.com/userguide2/tutorial/static_pages.html.
+
 <?php
 echo "Hello World About Page";
--- application/views/Templates/Header.php
+++ application/views/Templates/Header.php
@@ -1,7 +1,9 @@
+//This is the View for template header.
+//It was written using a CodeIgnitor tutorial found here:- https://codeigniter.com/userguide2/tutorial/static_pages.html.
+
 <html>
 <head>
 	<title><?php echo $title ?> - CodeIgniter 2 Tutorial</title>
 </head>
 <body>
 	<h1>CodeIgniter 2 Tutorial</h1>
-    
