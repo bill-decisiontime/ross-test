93531ee40ebea11fc4104d90a4f532cf6e7d954e
ross-teamsolutionz <ross.walker@teamsolutionz.com>
2017-02-09 16:44:27 +0000

    .

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,11 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-//Auto load for the languages class
-$autoload['libraries'] = array('database', 'form_validation', 'session');
-
-//Auto load for the shopping cart Class.
-//$autoload['libraries'] = array('cart', 'database');
+$autoload['libraries'] = array('cart', 'database');
 
 
 /*
@@ -68,7 +64,7 @@ $autoload['libraries'] = array('database', 'form_validation', 'session');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url', 'form', 'html');
+$autoload['helper'] = array('url', 'form');
 
 
 /*
--- application/views/Templates/Header.php
+++ application/views/Templates/Header.php
@@ -3,10 +3,9 @@
 //It was written using a CodeIgnitor tutorial found here:- https://codeigniter.com/userguide2/tutorial/static_pages.html.
 -->
 
-<!doctype html>
-
-<html lang="en">
+<html>
 <head>
+<<<<<<< HEAD
   <meta charset="utf-8">
 
   <title>"Example</title>
@@ -19,5 +18,9 @@
   <!--[if lt IE 9]>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
   <![endif]-->
+=======
+	<title><?php echo $title ?> - CodeIgniter 2 Tutorial</title>
+>>>>>>> parent of 993adfb... Start of Language Class Tutorial
 </head>
 <body>
+	<h1>CodeIgniter 2 Tutorial</h1>
