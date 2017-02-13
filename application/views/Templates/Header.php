dc1756f1f25f0c005e516dddf7e71c46705baa59
ross-teamsolutionz <ross.walker@teamsolutionz.com>
2017-02-13 09:10:30 +0000

    Revert "Revert "Start of Language Class Tutorial""
    This reverts commit f76917882d38a86559532c03e7dc595c23ae9901.

--- application/views/Templates/Header.php
+++ application/views/Templates/Header.php
@@ -5,7 +5,7 @@
 
 <html>
 <head>
-
+<<<<<<< HEAD
   <meta charset="utf-8">
 
   <title>"Example</title>
@@ -18,9 +18,9 @@
   <!--[if lt IE 9]>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
   <![endif]-->
-
+=======
 	<title><?php echo $title ?> - CodeIgniter 2 Tutorial</title>
-    
+>>>>>>> parent of 993adfb... Start of Language Class Tutorial
 </head>
 <body>
 	<h1>CodeIgniter 2 Tutorial</h1>
